4531e74b86e3aaaec1f38dd2748d5e509f92355b
Elle <lenash789@gmail.com>
2026-03-13 17:30:13 +0900

    modify

--- calculator.py
+++ calculator.py
@@ -2,11 +2,11 @@
 def add(a, b):
 	return a+b
 
-def subtract (a, b):
+def subtract(a, b):
 	return a-b
 
-def multiply (a, b):
+def multiply(a, b):
 	return a*b
 
-def divide_free (a, b):
+def divide_free(a, b):
 	return a/b
