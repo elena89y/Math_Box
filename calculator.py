fc2382195faa7efd17b558778368b15a82845e42
Elle <lenash789@gmail.com>
2026-03-13 17:29:42 +0900

    modify

--- calculator.py
+++ calculator.py
@@ -2,11 +2,11 @@
 def add(a, b):
 	return a+b
 
-def subtract (a, b):
+def subtract(a, b):
 	return a-b
 
-def multiply (a, b):
+def multiply(a, b):
 	return a*b
 
-def divide_premium (a, b):
+def divide_premium(a, b):
 	return a/b
